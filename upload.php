dd6ca597520bb1aad23ce32463a5b99c66525f93
Auto Hima <autohimas@gmail.com>
2026-01-18 04:19:34 +0200

    Update file upload logic

--- upload.php
+++ upload.php
@@ -1,9 +1,16 @@
 <?php
-$dir = __DIR__ . '/uploads/';
-if (!is_dir($dir)) mkdir($dir);
 
-if (!empty($_FILES['image'])) {
-    $name = time() . '_' . $_FILES['image']['name'];
-    move_uploaded_file($_FILES['image']['tmp_name'], $dir . $name);
-    echo "Uploaded: <a href='uploads/$name'>$name</a>";
+$uploadDir = __DIR__ . '/uploads/';
+
+if (!is_dir($uploadDir)) {
+    mkdir($uploadDir, 0777, true);
+}
+
+$fileName = time() . '_' . basename($_FILES['file']['name']);
+$target = $uploadDir . $fileName;
+
+if (move_uploaded_file($_FILES['file']['tmp_name'], $target)) {
+    echo "Uploaded: $fileName";
+} else {
+    echo "Upload failed";
 }
