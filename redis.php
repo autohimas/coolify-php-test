9a1d04ce8ac1858d4c989704509f13e551452ced
Auto Hima <autohimas@gmail.com>
2026-01-18 03:52:02 +0200

    Update file upload logic

--- redis.php
+++ redis.php
@@ -5,4 +5,13 @@ $redis->connect(
     '6379'
 );
 
+$pass = 'wJwoa635BhCV7QkMei3vnf7z4xH6WbxYdbDZY2ja7jU1FeeXIoJ6Lx4UxvHXrXIW';
+
+$redis = new Redis();
+$redis->connect($host, $port);
+
+if ($pass) {
+ $redis->auth($pass);
+}
+
 $redis->set("test_key", "Coolify Redis Works!");
