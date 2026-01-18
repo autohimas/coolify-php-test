770533e11d06e4c6bc9166720b56850b25dea5ba
Auto Hima <autohimas@gmail.com>
2026-01-18 03:53:20 +0200

    Update file upload logic

--- redis.php
+++ redis.php
@@ -1,15 +1,12 @@
 <?php
+
+
+$pass = 'wJwoa635BhCV7QkMei3vnf7z4xH6WbxYdbDZY2ja7jU1FeeXIoJ6Lx4UxvHXrXIW';
 $redis = new Redis();
 $redis->connect(
     '10.0.1.4',
     '6379'
 );
-
-$pass = 'wJwoa635BhCV7QkMei3vnf7z4xH6WbxYdbDZY2ja7jU1FeeXIoJ6Lx4UxvHXrXIW';
-
-$redis = new Redis();
-$redis->connect($host, $port);
-
 if ($pass) {
  $redis->auth($pass);
 }
